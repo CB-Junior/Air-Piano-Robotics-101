5984094e5a10a53204826feaeb327b041d0b7758
Nalin Luthra <nalin.luthra@gmail.com>
2020-10-20 21:10:09 +0530

    Add files via upload

---
+++ air_piano_live.pde
@@ -0,0 +1,13 @@
+import processing.sound.*; // Used to play with sound
+import processing.serial.*; // Used for communication with Ardunio
+
+
+SoundFile[] file = new SoundFile[13];
+Serial port;
+
+void setup(){
+  size(640,350);
+  background(255);
+
+  
+}
---
+++ live_air_piano.ino
@@ -0,0 +1,32 @@
+void setup() {
+  Serial.begin(9600);
+  pinMode(8,INPUT);
+  pinMode(9,INPUT);
+  pinMode(10,INPUT);
+  pinMode(11,INPUT);
+}
+long a; // Long is exactly same as int only difference between long and 
+// int is the range which they can store value 
+// random function returns long so we are using long to store it
+
+boolean flag1 = false;
+
+void loop() {
+  if(digitalRead(8) == 0){
+      a = 0;
+      Serial.println(a);
+    }  
+  if(digitalRead(9) == 0){
+      a = 1;
+      Serial.println(a);
+    }  
+  if(digitalRead(10) == 0){
+      a = 2;
+      Serial.println(a);
+    }  
+  if(digitalRead(11) == 0){
+    a = 3;
+      Serial.println(a);
+    }  
+    delay(50);
+}
